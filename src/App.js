f0cbd643a38f654f17d10ad5862667597495abbb
joshmuri <murikij@gmail.com>
2024-02-08 00:24:07 +0300

    Final edits

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { details: [], }
+
+  componentDidMount() {
+    axios.get('http://localhost:8000')
+      .then(res => {
+        this.setState({
+          details: res.data
+        });
+      })
+      .catch(err => {
+        console.error("Error fetching data:", err);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <header>Data Generated From Django</header>
+        <hr></hr>
+        {this.state.details.map((output, id) => ( // Correct arrow function syntax
+          <div key={id}>
+            <div>
+              <h2>{output.employee}</h2> {/* Access properties directly from 'output' */}
+              <h3>{output.department}</h3> {/* Access properties directly from 'output' */}
+            </div>
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default App;
